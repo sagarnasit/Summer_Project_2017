c12abf86190d0e7338a467b99ac9142d11a2365a
sagarnasit <sagarnasit@gmail.com>
2017-06-30 15:22:26 +0530

    User Auth Fully Complete

--- resources/views/Boot/layouts/header.blade.php
+++ resources/views/Boot/layouts/header.blade.php
@@ -4,100 +4,84 @@
 
                   <div class="pull-right user-option">
                         <ul class="list-inline">
-                              <li class="module-name"><a href="wishlist.html"><i class="fa fa-heart-o fa-2x color-scheme-2 margin-right-15"></i></a></li>
-                              <li class="margin-clear"><a href="compare.html"><i class="fa fa-retweet fa-2x color-scheme-2"></i><span class="paira-compare-count">0</span></a></li>
-                              <li class="dropdown cart-menu-body paira-cart-menu-body margin-clear">
-                                    <a href="cart.html" class="padding-bottom-15"><i class="fa fa-shopping-cart fa-2x color-scheme-2"></i><span class="paira-cart-item-count">0</span><span class="for-mobile-tab"><span class="paira-cart-total-price"><span class="money font-bold" data-currency-usd="$0.00">$500.00</span></span></span></a>
-                                    <div class="paira-cartNotEmpty cart-not-empty"  style="display: none;">
-                                          <ul class="dropdown-menu margin-top-5 padding-bottom-15">
-                                                <li class="text-center padding-top-5">
-                                                      <strong class="text-uppercase letter-spacing-2">Recently added 3 Items</strong>
-                                                </li>
-                                                <hr class="divider-small">
-                                                <li>
-                                                      <ul class="list-unstyled margin-clear padding-clear paira-cart-item-list">
-                                                            <li class="cart-menu-line-item padding-left-15 padding-right-15">
-                                                                  <a href="#" class="pull-left"><img src="assets/images/header/themetidy-paira-framework-boot-responsive-html-template-menu-1.jpg" alt=""></a>
-                                                                  <div class="pull-left">
-                                                                        <a href="#" class="cart-menu-line-title pull-left margin-left-10 font-size-14 letter-spacing-2 margin-bottom-5 font-bold">Converse Tecnicah Shoe</a><br>
-                                                                        <a href="#" class="cart-menu-line-title pull-left margin-left-10 font-size-14 letter-spacing-2 margin-bottom-5 font-bold">S - Black - Foam</a><br>
-                                                                        <p class=""><strong class="cart-menu-line-quantity font-size-14 margin-left-10 letter-spacing-2 font-bold">1</strong> x <strong class="cart-menu-line-price"><span class="money" data-currency-usd="$250.00">$250.00</span></strong></p>
-                                                                        <div class="pull-left cart-menu-line-cal text-right">
-                                                                              <a href="#" class="cart-menu-item-remove pull-right margin-bottom-20" data-varient-id="3561641988"><i class="fa fa-times font-color-black fa-2x"></i></a>
-                                                                        </div>
-                                                                  </div>
-                                                            </li>
-                                                            <li class="cart-menu-line-item padding-left-15 padding-right-15">
-                                                                  <a href="#" class="pull-left"><img src="assets/images/header/themetidy-paira-framework-boot-responsive-html-template-menu-2.jpg" alt=""></a>
-                                                                  <div class="pull-left">
-                                                                        <a href="#" class="cart-menu-line-title pull-left margin-left-10 font-size-14 letter-spacing-2 margin-bottom-5 font-bold">Converse Tecnicah Shoe</a><br>
-                                                                        <a href="#" class="cart-menu-line-title pull-left margin-left-10 font-size-14 letter-spacing-2 margin-bottom-5 font-bold">S - Black - Foam</a><br>
-                                                                        <p class=""><strong class="cart-menu-line-quantity font-size-14 margin-left-10 letter-spacing-2 font-bold">1</strong> x <strong class="cart-menu-line-price"><span class="money" data-currency-usd="$250.00">$250.00</span></strong></p>
-                                                                        <div class="pull-left cart-menu-line-cal text-right">
-                                                                              <a href="#" class="cart-menu-item-remove pull-right margin-bottom-20" data-varient-id="3561641988"><i class="fa fa-times font-color-black fa-2x"></i></a>
-                                                                        </div>
-                                                                  </div>
-                                                            </li>
-                                                            <li class="cart-menu-line-item padding-left-15 padding-right-15">
-                                                                  <a href="#" class="pull-left"><img src="assets/images/header/themetidy-paira-framework-boot-responsive-html-template-menu-3.jpg" alt=""></a>
-                                                                  <div class="pull-left">
-                                                                        <a href="#" class="cart-menu-line-title pull-left margin-left-10 font-size-14 letter-spacing-2 margin-bottom-5 font-bold">Converse Tecnicah Shoe</a><br>
-                                                                        <a href="#" class="cart-menu-line-title pull-left margin-left-10 font-size-14 letter-spacing-2 margin-bottom-5 font-bold">S - Black - Foam</a><br>
-                                                                        <p class=""><strong class="cart-menu-line-quantity font-size-14 margin-left-10 letter-spacing-2 font-bold">1</strong> x <strong class="cart-menu-line-price"><span class="money" data-currency-usd="$250.00">$250.00</span></strong></p>
-                                                                        <div class="pull-left cart-menu-line-cal text-right">
-                                                                              <a href="#" class="cart-menu-item-remove pull-right margin-bottom-20" data-varient-id="3561641988"><i class="fa fa-times font-color-black fa-2x"></i></a>
-                                                                        </div>
-                                                                  </div>
-                                                            </li>
-                                                            <li class="divider margin-bottom-20"></li>
-                                                      </ul>
-                                                </li>
-                                                <li class="padding-right-15 padding-left-15">
-                                                      <strong class="text-uppercase"><span class="pull-left">Total:</span> <span class="paira-cart-total-price pull-right"><span class="money" data-currency-usd="$300.00">$300.00</span></span></strong>
-                                                </li>
-                                                <li class="cart-menu-buttons pull-left margin-top-20">
-                                                      <a href="cart.html" class="btn btn-success text-capitalize pull-left">View Cart</a>
-                                                      <a href="#" class="btn btn-success text-capitalize pull-right">Checkout</a>
-                                                </li>
-                                          </ul>
-                                    </div>
-                                    <div class="paira-cartEmpty">
-                                          <ul class="dropdown-menu margin-top-5">
-                                                <li class="text-center padding-top-5">
-                                                      <strong class="text-uppercase letter-spacing-2">Recently added 0 Item</strong>
-                                                </li>
-                                                <hr class="divider-small">
-                                                <hr class="margin-bottom-10">
-                                                <li class="cart-menu-line-item text-center">
-                                                      Your cart is currently empty
-                                                </li>
-                                                <hr class="margin-bottom-10">
-                                                <li class="cart-menu-line-item text-center margin-bottom-20">
-                                                      <a href="collection.html" class="text-uppercase color-scheme-2 font-bold">Please shop now</a>
-                                                </li>
-                                          </ul>
-                                    </div>
-                              </li>
+                              @if (!Auth::guard('web_seller')->guest())
+                                    <li class="dropdown cart-menu-body paira-cart-menu-body margin-clear">
+                                          <a href="cart.html" class="padding-bottom-15"><i
+                                                        class="fa fa-shopping-cart fa-2x color-scheme-2"></i><span
+                                                        class="paira-cart-item-count">0</span><span
+                                                        class="for-mobile-tab"><span
+                                                              class="paira-cart-total-price"><span
+                                                                    class="money font-bold"
+                                                                    data-currency-usd="$0.00">$500.00</span></span></span></a>
+                                          <div class="paira-cartNotEmpty cart-not-empty" style="display: none;">
+                                                <ul class="dropdown-menu margin-top-5 padding-bottom-15">
+                                                      <li class="text-center padding-top-5">
+                                                            <strong class="text-uppercase letter-spacing-2">Recently
+                                                                  added 3
+                                                                  Items</strong>
+                                                      </li>
+                                                      <hr class="divider-small">
+
+                                                      <li class="padding-right-15 padding-left-15">
+                                                            <strong class="text-uppercase"><span
+                                                                          class="pull-left">Total:</span> <span
+                                                                          class="paira-cart-total-price pull-right"><span
+                                                                                class="money"
+                                                                                data-currency-usd="$300.00">$300.00</span></span></strong>
+                                                      </li>
+                                                      <li class="cart-menu-buttons pull-left margin-top-20">
+                                                            <a href="cart.html"
+                                                               class="btn btn-success text-capitalize pull-left">View
+                                                                  Cart</a>
+                                                            <a href="#"
+                                                               class="btn btn-success text-capitalize pull-right">Checkout</a>
+                                                      </li>
+                                                </ul>
+                                          </div>
+
+                                    </li>
+                              @endif
                         </ul>
                   </div>
-                  <div class="pull-right user-account">
-                        <ul class="list-inline">
-                              <li class="btn-group">
-                                    <a type="button" href="#" class="dropdown-toggle font-bold" data-toggle="dropdown" aria-expanded="false">
-                                          <span class="selected text-uppercase" data-id="flag-USD"><span class="usd-fl">My account</span></span>
-                                          <span class="fa fa-angle-down margin-left-5"></span>
-                                    </a>
-                                    <ul class="dropdown-menu dropdown-menu-left margin-clear" role="menu">
-                                          <li><a href="login.html" data-toggle="modal" data-target="#paira-login" class="font-bold">Login</a></li>
-                                          <li><a href="register.html" data-toggle="modal" data-target="#paira-register" class="font-bold">Register</a></li>
-                                    </ul>
-                              </li>
-                        </ul>
-                  </div>
+                  @if (Auth::guard()->guest())
+                        <div class="pull-right user-account">
+                              <ul class="list-inline">
+                                    <li class="btn-group">
+                                          <a type="button" href="/login" class="dropdown-toggle font-bold">
+                                                <span class="selected text-uppercase" data-id="flag-USD"><span
+                                                              class="usd-fl">Login</span></span>
+                                          </a>|
+                                          <a type="button" href="/register" class="dropdown-toggle font-bold"
+                                             >
+                                                <span class="selected text-uppercase" data-id="flag-USD"><span
+                                                              class="usd-fl">SignUp</span></span>
+                                          </a>
+
+                                    </li>
+                              </ul>
+                        </div>
+                  @else
+                        <div class="pull-right user-account">
+                              <ul class="list-inline">
+                                    <li class="btn-group">
+                                          <a type="button" href="/logout" class="dropdown-toggle font-bold"onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();" >
+                                                <span class="selected text-uppercase" data-id="flag-USD"><span
+                                                              class="usd-fl">logout</span></span>
+                                          </a>
+                                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                {{ csrf_field() }}
+                                          </form>
+
+
+                                    </li>
+                              </ul>
+                        </div>
+                  @endif
             </div>
       </section>
       @include('Boot.layouts.nav')
 </header>
 <div class="">
-       <hr class="myHr">
+      <hr class="myHr">
 </div>
