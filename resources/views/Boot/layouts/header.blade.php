a3a77c83abfbb58643e17a8d3305d8a4b7c04445
anil1408 <tuduanil14@outlook.com>
2017-08-16 13:03:29 +0530

    dropdown added for managing profile

--- resources/views/Boot/layouts/header.blade.php
+++ resources/views/Boot/layouts/header.blade.php
@@ -1,79 +1,79 @@
 <header>
-      <section class="header-top">
-            <div class="container-fluid">
-                  <div class="pull-right user-option">
-                        <ul class="list-inline">
-                              <li class="dropdown cart-menu-body paira-cart-menu-body margin-clear">
-                                    <a href="/cart" class="padding-bottom-15"><i
-                                                  class="fa fa-shopping-cart fa-2x color-scheme-2"></i><span
-                                                  class="paira-cart-item-count">{{Cart::count()}}</span><span
-                                                  class="for-mobile-tab"><span
-                                                        class="paira-cart-total-price"><span
-                                                              class="money font-bold"
-                                                              data-currency-usd="$0.00">Rs. {{Cart::total()}}</span></span></span></a>
+  <section class="header-top">
+    <div class="container-fluid">
+      <div class="pull-right user-option">
+        <ul class="list-inline">
+          <li class="dropdown cart-menu-body paira-cart-menu-body margin-clear">
+            <a href="/cart" class="padding-bottom-15"><i
+              class="fa fa-shopping-cart fa-2x color-scheme-2"></i><span
+              class="paira-cart-item-count">{{Cart::count()}}</span><span
+              class="for-mobile-tab"><span
+              class="paira-cart-total-price"><span
+              class="money font-bold"
+              data-currency-usd="$0.00">Rs. {{Cart::total()}}</span></span></span></a>
 
 
-                              </li>
+            </li>
 
-                        </ul>
-
-
-                  </div>
-
-                  @if (Auth::guard('web')->guest())
-                        <div class="pull-right user-account">
-                              <ul class="list-inline">
-                                    <li class="btn-group">
-                                          <a type="button" href="/login" class="dropdown-toggle font-bold">
-                                                <span class="selected text-uppercase" data-id="flag-USD"><span
-                                                              class="usd-fl">Login</span></span>
-                                          </a>|
-                                          <a type="button" href="/register" class="dropdown-toggle font-bold">
-                                                <span class="selected text-uppercase" data-id="flag-USD"><span
-                                                              class="usd-fl">SignUp</span></span>
-                                          </a>
-
-                                    </li>
-                              </ul>
-                        </div>
-
-                  @else
-                        <div class="pull-right user-account">
-                            <ul class="list-inline">
-                            <li class="btn-group">
-                                <a type="button" href="#" class="dropdown-toggle font-bold" data-toggle="dropdown" aria-expanded="false">
-                                    <span class="selected text-uppercase" data-id="flag-USD"><span class="usd-fl">My account</span></span>
-                                    <span class="fa fa-angle-down margin-left-5"></span>
-                                </a>
-                                <ul class="dropdown-menu dropdown-menu-left margin-clear" role="menu">
-                                    <li><a href="/your-account" data-toggle="modal" data-target="#paira-login" class="font-bold">My Profile</a></li>
-                                    <li class="btn-group">
-                                          <a type="button" href="/logout" class="dropdown-toggle font-bold"onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();" >
-                                                <span class="selected text-uppercase" data-id="flag-USD"><span
-                                                              class="usd-fl">logout</span></span>
-                                          </a>
-                                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                {{ csrf_field() }}
-                                          </form>
+          </ul>
 
 
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
+        </div>
 
-                            
-                        </div>
-                  @endif
+        @if (Auth::guard('web')->guest())
+        <div class="pull-right user-account">
+          <ul class="list-inline">
+            <li class="btn-group">
+              <a type="button" href="/login" class="dropdown-toggle font-bold">
+                <span class="selected text-uppercase" data-id="flag-USD"><span
+                  class="usd-fl">Login</span></span>
+                </a>|
+                <a type="button" href="/register" class="dropdown-toggle font-bold">
+                  <span class="selected text-uppercase" data-id="flag-USD"><span
+                    class="usd-fl">SignUp</span></span>
+                  </a>
 
-
-
-
+                </li>
+              </ul>
             </div>
-      </section>
-      @include('Boot.layouts.nav')
-</header>
-<div class="">
-      <hr class="myHr">
-</div>
+
+            @else
+            <div class="pull-right user-account">
+              <ul class="list-inline">
+                <li class="btn-group">
+                  <a type="button" href="#" class="dropdown-toggle font-bold" data-toggle="dropdown" aria-expanded="false">
+                    <span class="selected text-uppercase" data-id="flag-USD"><span class="usd-fl">My account</span></span>
+                    <span class="fa fa-angle-down margin-left-5"></span>
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-left margin-clear" role="menu">
+                    <li><a href="/your-account">My Profile</a></li>
+                    <li class="btn-group">
+                      <a type="button" href="/logout" class="dropdown-toggle font-bold"onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();" >
+                      <span class="selected text-uppercase" data-id="flag-USD"><span
+                        class="usd-fl">logout</span></span>
+                      </a>
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                      </form>
+
+
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+
+              
+            </div>
+            @endif
+
+
+
+
+          </div>
+        </section>
+        @include('Boot.layouts.nav')
+      </header>
+      <div class="">
+        <hr class="myHr">
+      </div>
